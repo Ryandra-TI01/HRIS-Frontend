72a04eb9ff75228809f0632b449e582988ccf453
Yossy123 <yossykusuma01@gmail.com>
2025-11-30 15:33:42 +0700

    feat: adding a logo to the navbar and adding a footer as well as changing the content description

---
+++ src/components/Logo.jsx
@@ -0,0 +1,23 @@
+import { useTheme } from "next-themes";
+
+export default function Logo({ size = 200 }) {
+  const { theme } = useTheme();
+
+  const logoSrc =
+    theme === "dark"
+      ? "/LogoHris-dark.png"
+      : "/LogoHris.png";
+
+  return (
+    <img
+      src={logoSrc}
+      alt="HRIS Sync Logo"
+      style={{
+        width: size,
+        height: "auto",
+        objectFit: "contain",
+        display: "block",
+      }}
+    />
+  );
+}
